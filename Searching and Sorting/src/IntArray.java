179e8f3f1f4900014b933dff1a7ef5c2ffcef1e3
quaoz <quaoz@protonmail.com>
2021-10-01 20:37:47 +0100

    searching and sorting
    Signed-off-by: quaoz <quaoz@protonmail.com>

---
+++ Searching and Sorting/src/IntArray.java
@@ -0,0 +1,109 @@
+public class IntArray {
+	private int[] array;
+
+	IntArray(int[] array) {
+		// copies the value to a new array with one extra element for a sentinel value
+		this.array = new int[array.length];
+		System.arraycopy(array, 0, this.array, 0, array.length);
+	}
+
+	public int[] getArray() {
+		return array;
+	}
+
+	public void setArray(int[] array) {
+		this.array = array;
+	}
+
+	public int search(int term, boolean contains) {
+		// adds the sentinel value
+		array[array.length - 1] = term;
+		int count = 0;
+
+		// loops until it reaches the term or sentinel value
+		while (term != array[count]) {
+			count++;
+		}
+
+		// returns false if the term was the sentinel value
+		if (count != array.length - 1) {
+			// -2 indicates that the value was not found
+			return -2;
+		}
+		// -1 indicates that the array contains the value
+		// the position is returned if it is asked for (contains is false)
+		return contains ? -1 : count;
+	}
+
+	public int[] bubbleSort(boolean reverse) {
+		boolean swapped = true;
+		int sortedCount = 0;
+
+		// loops until it makes a pass without swapping something
+		while (swapped) {
+			swapped = false;
+
+			// loops through each element in the array
+			for (int i = 0; i < array.length - 1 - sortedCount; i++) {
+				// swaps the elements without storing the in a temporary variable
+				if ((reverse && array[i] < array[i + 1]) || (!reverse && array[i] > array[i + 1])) {
+					array[i] = (array[i] + array[i + 1]) - (array[i + 1] = array[i]);
+					swapped = true;
+				}
+			}
+			sortedCount++;
+		}
+
+		return array;
+	}
+
+	// as merge sort recursively calls itself we have to overload it without any parameters to get it to operate
+	// on the array which is part of this class, we can now call intArray.mergeSort() instead of having to do
+	// intArray.mergeSort(intArray.getArray(), intArray.getArray().length), this also allows us to return the sorted
+	// array as we do with the bubbleSort function
+	public int[] mergeSort() {
+		mergeSort(array, array.length);
+		return array;
+	}
+
+	public static void mergeSort(int[] array, int pos) {
+		if (pos < 2) {
+			return;
+		}
+
+		// creates two new arrays
+		int mid = pos / 2;
+		int[] leftArray = new int[mid];
+		int[] rightArray = new int[pos - mid];
+
+		// splits the content of the main array
+		System.arraycopy(array, 0, leftArray, 0, mid);
+
+		if (pos - mid >= 0) {
+			System.arraycopy(array, mid, rightArray, 0, pos - mid);
+		}
+
+		// recursively sorts the arrays
+		mergeSort(leftArray, mid);
+		mergeSort(rightArray, pos - mid);
+
+		// merges the resulting arrays
+		merge(array, leftArray, rightArray, mid, pos - mid);
+	}
+
+	public static void merge(int[] array, int[] leftArray, int[] rightArray, int left, int right) {
+		int i = 0, j = 0, k = 0;
+
+		while (i < left && j < right) {
+			array[k++] = leftArray[i] <= rightArray[j]
+					? leftArray[i++]
+					: rightArray[j++];
+		}
+		while (i < left) {
+			array[k++] = leftArray[i++];
+		}
+		while (j < right) {
+			array[k++] = rightArray[j++];
+		}
+	}
+}
---
+++ Searching and Sorting/src/Timer.java
@@ -0,0 +1,100 @@
+public class Timer {
+	private long startTime;
+	private long endTime;
+	private boolean timerRunning;
+	// -1 - not running, 0 - nanoseconds, 1 - milliseconds
+	private int timerMode;
+
+	Timer() {
+		this.timerMode = -1;
+		this.startTime = 0;
+		this.endTime = 0;
+		this.timerRunning = false;
+	}
+
+	// returns whether the timer is still running
+	public boolean isTimerRunning() {
+		return timerRunning;
+	}
+
+	// returns the start time
+	public long getStartTime() {
+		return startTime;
+	}
+
+	// returns the end time
+	public long getEndTime() {
+		return endTime;
+	}
+
+	// stores the current time in nanoseconds as the start time
+	public void startTimerNano() throws RuntimeException {
+		// throws an exception if the timer was already running when the method was called
+		if (!timerRunning) {
+			startTime = System.nanoTime();
+		} else {
+			throw new RuntimeException("Attempted to start the timer while it was running");
+		}
+
+		timerMode = 0;
+		timerRunning = true;
+	}
+
+	// stores the current time in milliseconds as the start time
+	public void startTimerMilli() throws RuntimeException {
+		// throws an exception if the timer was already running when the method was called
+		if (!timerRunning) {
+			startTime = System.currentTimeMillis();
+		} else {
+			throw new RuntimeException("Attempted to start the timer while it was running");
+		}
+
+		timerRunning = true;
+		timerMode = 1;
+	}
+
+	// stores the current time as the end time
+	public void stopTimer() throws RuntimeException {
+		// throws an exception if the timer wasn't running when the method was called
+		if (timerRunning) {
+			// stops the timer in the correct time unit
+			if (timerMode == 0) {
+				endTime = System.nanoTime();
+			} else {
+				endTime = System.currentTimeMillis();
+			}
+		} else {
+			throw new RuntimeException("Attempted to stop the timer while it wasn't running");
+		}
+
+		timerRunning = false;
+	}
+
+	// resets the timer
+	public void resetTimer() throws RuntimeException {
+		// throws an exception if the timer was running when the method was called
+		if (!timerRunning) {
+			startTime = 0;
+			endTime = 0;
+			timerMode = -1;
+		} else {
+			throw new RuntimeException("Attempted to reset the timer while it was running");
+		}
+	}
+
+	// returns the difference between the start and end times or minus one if the timer hasn't been stopped
+	public long getElapsedTime() {
+		// throws an exception if the timer was running when the method was called
+		if (!timerRunning) {
+			return endTime - startTime;
+		} else {
+			throw new RuntimeException("Attempted to get the elapsed time while the timer was running");
+		}
+	}
+
+	// wrapper for stopTimer and getElapsedTime
+	public long stopAndGetElapsedTime() {
+		stopTimer();
+		return getElapsedTime();
+	}
+}
