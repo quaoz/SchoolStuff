678c74204657912e2f503661a86e6dcaa05dc036
quaoz <quaoz@protonmail.com>
2021-10-22 18:26:10 +0100

    add PrintProgress.java
    Signed-off-by: quaoz <quaoz@protonmail.com>

---
+++ src/main/java/com/github/quaoz/common/console/PrintProgress.java
@@ -0,0 +1,42 @@
+package com.github.quaoz.common.console;
+
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Class to print the program progress to the console
+ */
+public class PrintProgress {
+
+	/**
+	 * Prints a stylised progress bar to the console
+	 *
+	 * <p> Modified version of this https://stackoverflow.com/a/39257969 answer </p>
+	 *
+	 * @param startTime The starting time in milliseconds
+	 * @param total     The total number of times this method will be called
+	 * @param current   The current number of this method call
+	 */
+	public static void printProgressBar(long startTime, long total, long current) {
+		long eta = current == 0
+				? 0
+				: (total - current) * (System.currentTimeMillis() - startTime) / current;
+
+		String etaHms = current == 0
+				? "N/A"
+				: String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(eta),
+				TimeUnit.MILLISECONDS.toMinutes(eta) % TimeUnit.HOURS.toMinutes(1),
+				TimeUnit.MILLISECONDS.toSeconds(eta) % TimeUnit.MINUTES.toSeconds(1));
+
+		int percent = (int) (current * 100 / total);
+
+		String string = "\r" +
+				String.format("%d%% [", percent) +
+				String.join("", Collections.nCopies(percent, "=")) + '>' +
+				String.join("", Collections.nCopies(100 - percent, " ")) + ']' +
+				String.join("", Collections.nCopies((int) (Math.log10(total)) - (int) (Math.log10(current)), " ")) +
+				String.format(" %d/%d, ETA: %s", current, total, etaHms);
+
+		System.out.print(string);
+	}
+}
