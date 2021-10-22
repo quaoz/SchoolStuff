2dc97e90cfed151ffd5bb1c9eb8718d468faa49d
quaoz <quaoz@protonmail.com>
2021-10-22 18:44:27 +0100

    WordGrid
    Signed-off-by: quaoz <quaoz@protonmail.com>

--- src/main/java/com/github/quaoz/tasks/wordgrid/WordGrid.java
+++ src/main/java/com/github/quaoz/tasks/wordgrid/WordGrid.java
@@ -7,7 +7,9 @@ class WordGrid {
 	private final Character[][] grid;
 
 	/**
-	 * @param string
+	 * Constructor
+	 *
+	 * @param string The string to construct the grid from
 	 */
 	WordGrid(@NotNull String string) {
 		this.grid = new Character[10][string.length()];
@@ -29,10 +31,24 @@ class WordGrid {
 		return grid[row][column];
 	}
 
+	/**
+	 * Sets the character at a specific location
+	 *
+	 * @param character The character to use
+	 * @param row       The row of the element to set
+	 * @param column    The column of the element to set
+	 */
 	public void setCharacter(Character character, int row, int column) {
 		grid[row][column] = character;
 	}
 
+	/**
+	 * Returns the requested column
+	 *
+	 * @param column The column to return
+	 *
+	 * @return String The column as a string
+	 */
 	public String getColumn(int column) {
 		StringBuilder columnString = new StringBuilder();
 
@@ -43,6 +59,13 @@ class WordGrid {
 		return columnString.toString();
 	}
 
+	/**
+	 * Returns a diagonal from the grid
+	 *
+	 * @param row The row to start the diagonal from
+	 *
+	 * @return String The characters going diagonally down and right from the specified row
+	 */
 	public String getDiagonal(int row) {
 		StringBuilder diagonalString = new StringBuilder();
 		int count = 0;
@@ -58,6 +81,13 @@ class WordGrid {
 		return diagonalString.toString();
 	}
 
+	/**
+	 * Searches for the first occurrence of a character
+	 *
+	 * @param character The character to find
+	 *
+	 * @return int[] The position of the requested character, {-1, -1} if it's not found
+	 */
 	public int[] find(Character character) {
 		int[] position = {-1, -1};
 
