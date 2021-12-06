83068f59f4fe93f1ff5cfaf06bca7da098a277fa
quaoz school <quaoz@protonmail.com>
2021-12-06 11:35:28 +0000

    up

--- src/main/java/com/github/quaoz/scripts/BubbleAndSelection.java
+++ src/main/java/com/github/quaoz/scripts/BubbleAndSelection.java
@@ -5,17 +5,9 @@ import com.github.quaoz.common.sorts.SelectionSort;
 
 public class BubbleAndSelection {
 	public static void main(String[] args) {
-		Integer[] array = new Integer[]{
-				1,
-				2,
-				3,
-				5,
-				4,
-				7,
-				6
-		};
+		Integer[] array = new Integer[]{1, 2, 3, 5, 4, 7, 6};
 
-		//BubbleSort.sort(array.clone());
+		BubbleSort.sort(array.clone());
 		System.out.println();
 		SelectionSort.sort(array.clone());
 	}
