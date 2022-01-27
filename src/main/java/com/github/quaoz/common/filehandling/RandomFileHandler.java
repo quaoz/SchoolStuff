638c6a125f61d1c23c84a0c166783ce2011eed15
quaoz school <quaoz@protonmail.com>
2022-01-27 10:13:50 +0000

    more file hell

--- src/main/java/com/github/quaoz/common/filehandling/RandomFileHandler.java
+++ src/main/java/com/github/quaoz/common/filehandling/RandomFileHandler.java
@@ -1,32 +1,51 @@
 package com.github.quaoz.common.filehandling;
 
+import org.jetbrains.annotations.Nullable;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class RandomFileHandler {
-	public static void main(String[] args) {
-		File file = new File("src/main/java/com/github/quaoz/scripts/testfile.txt");
 
-		for (int i = 0; i < 10; i++) {
-			randomRead(file, i);
-		}
-	}
+	/**
+	 * Reads a character at a specified position from a file
+	 *
+	 * @param file  The file to read from
+	 * @param pos   The position to read at
+	 *
+	 * @return The character at that postion
+	 */
+	public static @Nullable Character randomRead(File file, long pos) {
+		try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rs")) {
+			randomAccessFile.seek(pos);
 
-	public static void randomRead(File file, int start) {
-		try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rws")) {
-			randomAccessFile.seek(start);
-			char letter = randomAccessFile.readChar();
-			System.out.println(letter);
+			// randomAccessFile.readChar() didn't work as it reads the first two bytes however the text file encoding
+			// used one byte per character, so instead we read the byte and cast it to a char
+			return (char) randomAccessFile.readByte();
 		} catch (IOException e) {
+			System.out.printf("Failed to read byte at %d in %s", pos, file);
 			e.printStackTrace();
 		}
+		return null;
 	}
 
-	public static void randomWrite(File file, char c) {
+	/**
+	 * Writes the given character to a specific position in a file
+	 *
+	 * @param file The file to write to
+	 * @param pos  The position to write at
+	 * @param c	   The character to write
+	 */
+	public static void randomWrite(File file, long pos, char c) {
 		try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rws")) {
-			randomAccessFile.writeChar(c);
+			// seeks to the given position
+			randomAccessFile.seek(pos);
+
+			// casts the char to a byte and writes it to the file
+			randomAccessFile.writeByte((byte) c);
 		} catch (IOException e) {
+			System.out.printf("Failed to write char %c at %d in %s", c, pos, file);
 			e.printStackTrace();
 		}
 	}
---
+++ src/main/java/com/github/quaoz/tasks/randomfiles/Main.java
@@ -0,0 +1,42 @@
+package com.github.quaoz.tasks.randomfiles;
+
+import com.github.quaoz.common.filehandling.RandomFileHandler;
+import com.github.quaoz.common.filehandling.SequentialFileHandler;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.Objects;
+
+public class Main {
+	public static void main(String[] args) {
+		File file = new File("src/main/java/com/github/quaoz/tasks/randomfiles/testfile.txt");
+
+		readEveryOther(file);
+		replaceSpaces(file);
+		readReverse(file);
+	}
+
+	private static void readEveryOther(@NotNull File file) {
+		// Jump forwards by two bytes until it reaches the end of the file
+		for (long i = 0; i < file.length(); i += 2) {
+			System.out.println(RandomFileHandler.randomRead(file, i));
+		}
+	}
+
+	private static void replaceSpaces(@NotNull File file) {
+		// Iterates from the start of the file to the end
+		for (long i = 0; i < file.length(); i++) {
+			// Replaces all spaces with x
+			if (Objects.equals(RandomFileHandler.randomRead(file, i), ' ')) {
+				RandomFileHandler.randomWrite(file, i, 'x');
+			}
+		}
+	}
+
+	private static void readReverse(@NotNull File file) {
+		// Iterates from the end of the file to the start
+		for (long i = file.length(); i >= 0; i--) {
+			System.out.println(RandomFileHandler.randomRead(file, i));
+		}
+	}
+}
